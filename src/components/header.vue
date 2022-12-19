fcea0d8f81eb207606ec1c7ccc78ffdbba6cdf67
dong0408 <250561787@qq.com>
2022-12-19 21:58:21 +0800

    feat:axios封装

--- src/components/header.vue
+++ src/components/header.vue
@@ -39,7 +39,7 @@ const input=ref('')
 }
 .right{
   display: flex;
-} 
+}  
 .left{
   display: flex;
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,26 +1,87 @@
-import axios, {AxiosRequestConfig,AxiosResponse} from "axios";
-const service =axios.create()
+import { rejects } from "assert";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { Message } from "element-plus";
+import { resolve } from "path";
 
- // Request interceptors
-service.interceptors.request.use(
-    (config: AxiosRequestConfig)=>{
-      return config;
-    },
-    (error:any)=>{
-        Promise.reject(error)
-    }
-);
+const baseURL = process.env.VITE_APP_BASE_API
 
-// Response interceptors
-service.interceptors.response.use(
-    // async
-    (response:AxiosResponse)=>{   
-        console.log(response);
-     return  response
-    },
-    (error:any)=>{
-        return Promise.reject(error)
-    }
+
+// Request interceptors
+const createService = (option: any) => {
+    const service = axios.create()
+    service.interceptors.request.use(
+        (config: AxiosRequestConfig) => {
+            if (option) {
+                config = { ...config, ...option }
+            }
+            return config;
+        },
+        (error: any) => {
+            Promise.reject(error)
+        }
     );
+    // Response interceptors
+    service.interceptors.response.use(
+        // async
+        (response: AxiosResponse) => {
+            console.log(response);
+            return response
+        },
+        (error: any) => {
+            return Promise.reject(error)
+        }
+    )
+    return service
+}
 
-    export default service
+//创建axios参数对象
+const createConfig = (config: any, data: Object) => {
+    const { method = 'GET', url, ...resetConfig } = config
+    const key = method.toUpperCase() === 'post' ? 'data' : 'params'
+    const _ = {
+        method,
+        url,
+        ...resetConfig,
+    }
+    _[key] = data
+    return _
+
+}
+
+const service = createService()
+
+export default function request(config: any, data: Object) {
+    const { muted = false } = config
+    const option = createConfig(config, data)
+    // const {$store}
+    return new Promise((resolve, rejects) => {
+        const defaultMsg = '出错了'
+        service(options).then(response => {
+            const { code, msg = defaultMsg } = response.data
+            if (code !== ResponseCode.OK) {
+
+                if (code === ResponseCode.TOKEN_INVALID) {
+                 if($store){
+                    $store.dispatch('logout')
+                 }
+                }
+                if(!muted){
+                    $message.warning(msg)
+                }
+                rejects(response)
+                return 
+            }
+            resolve(response)
+        }).catch(error =>{
+            if(!muted){
+                $message.warning(defaultMsg)
+            }
+            reject(error)
+
+        })
+    })
+}
+
+
+
+
